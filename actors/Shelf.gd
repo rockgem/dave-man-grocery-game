95a798398b8b6e5a6b1d8112fe8d53560cc87167
Rock Gementiza <63050183+rockgem@users.noreply.github.com>
2025-04-09 00:03:41 +0800

    generating shit

--- actors/Shelf.gd
+++ actors/Shelf.gd
@@ -6,4 +6,13 @@ func _ready() -> void:
 
 
 func generate_level():
-	pass
+	var values = [0, 1, 2, 3]
+	values.shuffle()
+	
+	for i in 3:
+		var rand_amount = randi_range(1, 5)
+		var slots_in_row = $Slots.get_child(i).get_children()
+		slots_in_row.shuffle()
+		
+		for j in rand_amount:
+			slots_in_row[j].put_item(values[i])
--- actors/Slot.gd
+++ actors/Slot.gd
@@ -25,3 +25,8 @@ func _ready() -> void:
 
 func put_item(item_type: int):
 	self.item_type = item_type
+	
+	if item_type == ITEM_TYPE.VACANT:
+		$Sprite2D.texture = null
+	else:
+		$Sprite2D.texture = load("res://reso/items/%s.tres" % ITEM_TYPE.find_key(item_type))
