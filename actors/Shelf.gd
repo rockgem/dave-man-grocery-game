2ed0d933e6dbbdb21c514aee1935c586bbc3c24c
Rock Gementiza <63050183+rockgem@users.noreply.github.com>
2025-04-08 23:51:54 +0800

    dsfsdf

---
+++ actors/Shelf.gd
@@ -0,0 +1,9 @@
+extends Sprite2D
+
+
+func _ready() -> void:
+	generate_level()
+
+
+func generate_level():
+	pass
---
+++ actors/Slot.gd
@@ -0,0 +1,27 @@
+@tool
+
+extends Area2D
+
+
+enum ITEM_TYPE{
+	MILK,
+	BREAD,
+	STRAWBERRY,
+	SUGAR,
+	VACANT
+}
+
+
+@export var item_type = ITEM_TYPE.VACANT
+
+
+func _ready() -> void:
+	if item_type == ITEM_TYPE.VACANT:
+		$Sprite2D.texture = null
+	else:
+		$Sprite2D.texture = load("res://reso/items/%s.tres" % ITEM_TYPE.find_key(item_type))
+
+
+
+func put_item(item_type: int):
+	self.item_type = item_type
